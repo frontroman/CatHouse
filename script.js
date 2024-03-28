24f647ec76e081749ade998c1397eb89b52934ed
xwave97 <roma.loboda.2000@mail.ru>
2024-03-28 22:41:23 +0300

    final

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let offset = 0;
 
 document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById("next").addEventListener("click", ()=>{
-        offset = offset + 1100;
-        if(offset > 2700){
+        offset = offset + 370;
+        if(offset > 2570){
             offset = 0;
         }
 
@@ -15,9 +15,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById("prev").addEventListener("click", ()=>{
-        offset = offset - 1100;
+        offset = offset - 370;
+        console.log(offset)
         if(offset < 0){
-            offset = 2200;
+            offset = 2590;
         }
 
         document.getElementById("carousel").style.left = -offset + "px";
@@ -35,3 +36,13 @@ for(let i = 0; i < questions.length; i++){
 
     })
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function(){
+        document.getElementById("nav").classList.toggle("open");
+        document.getElementById("header").classList.toggle("open");
+    })
+});
